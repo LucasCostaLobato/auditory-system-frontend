42df9ecbf43e496c89319ccb1daa9c60c58cdfd0
Lucas Lobato <lobato@lisha.ufsc.br>
2026-02-09 21:18:35 -0300

    Add explanation sidebars of middle ear page

--- src/components/explanations/MiddleEarDynamicExplanation.jsx
+++ src/components/explanations/MiddleEarDynamicExplanation.jsx
@@ -1,4 +1,5 @@
 import { useLanguage } from '../../contexts/LanguageContext';
+import responseImg from '../../assets/sidebar_middleear_response.png';
 import './ExplanationSidebar.css';
 
 const MiddleEarDynamicExplanation = () => {
@@ -8,7 +9,16 @@ const MiddleEarDynamicExplanation = () => {
     <div className="explanation-sidebar">
       <h3 className="explanation-title">{t('middleEar.dynamicExplanationTitle')}</h3>
       <div className="explanation-content">
-        <p>{t('middleEar.dynamicExplanationContent')}</p>
+        <ul>
+          <li>{t('middleEar.dynamicExplanationBullet1')}</li>
+          <li>{t('middleEar.dynamicExplanationBullet2')}</li>
+        </ul>
+        <img src={responseImg} alt="Middle ear response" className="explanation-image middleear-response-image" />
+        <ul>
+          <li>{t('middleEar.dynamicExplanationBullet3')}</li>
+          <li>{t('middleEar.dynamicExplanationBullet4')}</li>
+          <li>{t('middleEar.dynamicExplanationBullet5')}</li>
+        </ul>
       </div>
     </div>
   );
--- src/components/explanations/MiddleEarFRFExplanation.jsx
+++ src/components/explanations/MiddleEarFRFExplanation.jsx
@@ -1,4 +1,5 @@
 import { useLanguage } from '../../contexts/LanguageContext';
+import frfImg from '../../assets/sidebar_middleear_frf.png';
 import './ExplanationSidebar.css';
 
 const MiddleEarFRFExplanation = () => {
@@ -8,7 +9,16 @@ const MiddleEarFRFExplanation = () => {
     <div className="explanation-sidebar">
       <h3 className="explanation-title">{t('middleEar.frfExplanationTitle')}</h3>
       <div className="explanation-content">
-        <p>{t('middleEar.frfExplanationContent')}</p>
+        <ul>
+          <li>{t('middleEar.frfExplanationBullet1')}</li>
+        </ul>
+        <img src={frfImg} alt="Middle ear FRF" className="explanation-image middleear-frf-image" />
+        <ul>
+          <li>{t('middleEar.frfExplanationBullet2')}</li>
+          <li>{t('middleEar.frfExplanationBullet3')}</li>
+          <li>{t('middleEar.frfExplanationBullet4')}</li>
+          <li>{t('middleEar.frfExplanationBullet5')}</li>
+        </ul>
       </div>
     </div>
   );
